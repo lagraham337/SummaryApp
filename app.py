044033b8f6464d4aa5eddc453047fa7dfc0676f9
Lauren Graham <lagraham337@gmail.com>
2022-10-16 12:10:39 -0700

    Added under construction banner to IMG

--- app.py
+++ app.py
@@ -4,6 +4,10 @@ import pandas as pd
 import numpy as np
 import random
 
+from htbuilder import HtmlElement, div, ul, li, br, hr, a, p, img, styles, classes, fonts
+from htbuilder.units import percent, px
+from htbuilder.funcs import rgba, rgb
+
 import nltk
 #nltk.download("punkt") # may need this if encounter an error
 
@@ -156,6 +160,8 @@ Note: The Clear button can be used to clear the text or input areas.
             st.write('Text was cleared.')
 
     if selected == 'Input IMG':
+        st.image("https://img.freepik.com/free-vector/coming-soon-construction-yellow-background-design_1017-25509.jpg?w=2000&t=st=1665945497~exp=1665946097~hmac=11c2d774f1d79ab624c56155e5b112091695207f74af553831f9e61030f52670")
+        
         # ANGELA WILL MAKE A MICROSERVICE TO PROCESS IMAGE FILES
         st.title("IMG 📸")
         st.write("Extract or summarize text from an image.")
@@ -188,6 +194,8 @@ Note: The Clear button can be used to clear the text or input areas.
         if clear:
             st.write('Upload was cleared.')
 
+        st.caption("Image by starline on [Freepik](https://www.freepik.com/free-vector/coming-soon-construction-yellow-background-design_8562867.htm#query=website%20under%20construction&position=15&from_view=keyword)")
+
 
     if selected == 'NER Checker':
         # NOT WORKING
@@ -211,5 +219,67 @@ Note: The Clear button can be used to clear the text or input areas.
         if clear:
             st.write('Text was cleared.')
 
+
+def image(src_as_string, **style):
+    return img(src=src_as_string, style=styles(**style))
+
+def link(link, text, **style):
+    return a(_href=link, _target="_blank", style=styles(**style))(text)
+
+def layout(*args):
+
+    style = """
+    <style>
+      # MainMenu {visibility: hidden;}
+      footer {visibility: hidden;}
+    </style>
+    """
+
+    style_div = styles(
+        left=0,
+        bottom=0,
+        margin=px(0, 0, 0, 0),
+        width=percent(100),
+        text_align="center",
+        height="60px",
+        opacity=0.6
+    )
+
+    style_hr = styles(
+    )
+
+    body = p()
+    foot = div(style=style_div)(hr(style=style_hr), body)
+
+    st.markdown(style, unsafe_allow_html=True)
+
+    for arg in args:
+        if isinstance(arg, str):
+            body(arg)
+        elif isinstance(arg, HtmlElement):
+            body(arg)
+
+    st.markdown(str(foot), unsafe_allow_html=True)
+
+def footer():
+    myargs = [
+        "<b>Made with</b>: Python 3.10 ",
+        link("https://www.python.org/", image('https://i.imgur.com/ml09ccU.png',
+        	width=px(18), height=px(18), margin= "0em")),
+        ", Streamlit ",
+        link("https://streamlit.io/", image('https://aws1.discourse-cdn.com/business7/uploads/streamlit/original/2X/f/f0d0d26db1f2d99da8472951c60e5a1b782eb6fe.png',
+        	width=px(24), height=px(25), margin= "0em")),    
+        # link("https://github.com/lagraham337", "Lauren Graham"),
+        "by",
+        link("https://github.com/lagraham337/SummaryApp", image('https://img.icons8.com/plasticine/344/github.png', width=px(24), height=px(25), margin= "0em")),
+        "Lauren Graham",
+        #br(),
+    ]
+    layout(*myargs)
+
+
 if __name__ == '__main__':
     main()
+
+if __name__ == "__main__":
+    footer()
